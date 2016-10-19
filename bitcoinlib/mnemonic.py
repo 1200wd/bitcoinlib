4b6f9c35178b98027e6abaf96150c25522803390
Lennart <lennart@1200wd.com>
2016-10-19 08:35:58 +0200

    Add checksum according to BIP39 standard

--- bitcoinlib/mnemonic.py
+++ bitcoinlib/mnemonic.py
@@ -20,6 +20,7 @@
 #
 
 import os
+import hashlib
 from encoding import change_base
 
 DEFAULT_LANGUAGE = 'dutch'
@@ -31,37 +32,47 @@ class Mnemonic:
         with open('%s/%s.txt' % (wldir,language), 'r') as f:
             self._wordlist = [w.strip() for w in f.readlines()]
 
+    @staticmethod
+    def checksum(data):
+        cs = len(data) / 4
+        return hashlib.sha256(data).hexdigest()[:cs]
+
     def word(self, index):
         return self._wordlist[index]
 
     def wordlist(self):
         return self._wordlist
 
-    def generate(self, strength=128):
+    def generate(self, strength=128, include_checksum=True):
         data = os.urandom(strength // 8)
+        if include_checksum:
+            data = data + self.checksum(data)
         return self.to_mnemonic(data)
 
-
-    def to_entropy(self, words, base_to=16):
+    def to_entropy(self, words, base_to=16, includes_checksum=True):
         wi = []
         for word in words:
             wi.append(self._wordlist.index(word))
         ent = change_base(wi, 2048, base_to)
+        if includes_checksum:
+            print ent
+            pass
         return ent
 
-    def to_mnemonic(self, data):
+    def to_mnemonic(self, data, include_checksum=True):
+        if include_checksum:
+            data = data + self.checksum(data)
         wi = change_base(data, 256, 2048)
         return [self._wordlist[i] for i in wi]
 
 
+
 if __name__ == '__main__':
     entsize = 32
-    mobj = Mnemonic()
-    wpl = mobj.generate(entsize)
-    print "Your password is: %s" % ' '.join(wpl)
-    print "A computer needs an avarage of %.2f tries to guess this password" % ((2 ** entsize) /2.0)
-
-    base = mobj.to_entropy(wpl, 256)
-    print "In HEX this is %s" % change_base(base, 256, 16, 8)
-
-    print "Convert back to base2048: %s" % mobj.to_mnemonic(base)
+    wpl = Mnemonic().generate(entsize)
+    print("Your password is: %s" % ' '.join(wpl))
+    print("A computer needs an avarage of %.2f tries to guess this password" % ((2 ** entsize) /2.0))
+    base = Mnemonic().to_entropy(wpl, 256)
+    print("In HEX this is %s" % change_base(base, 256, 16, 8))
+    print("Checksum is %s" % Mnemonic().checksum(base))
+    print("Convert back to base2048: %s" % Mnemonic().to_mnemonic(base))
