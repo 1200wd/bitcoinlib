2374da3bdc9e6f9e075a1db21808119b81d9eb54
Lennart Jongeneel <lennart@1200wd.com>
2017-03-04 19:43:04 +0100

    [REF] Update versionnumber

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ kwargs['install_requires'] = install_requires
 
 setup(
       name='bitcoinlib',
-      version='0.3.0.15',
+      version='0.3.0.16',
       description='Bitcoin and Other cryptocurrency Library',
       long_description=readmetxt,
       classifiers=[
