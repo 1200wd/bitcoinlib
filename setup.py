976a2240d4618420df54155237c2ea02801a336a
Lennart Jongeneel <lennart@1200wd.com>
2017-05-07 23:04:17 +0200

    [UPD] PyPi version to 18

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ kwargs['install_requires'] = install_requires
 
 setup(
       name='bitcoinlib',
-      version='0.3.0.17',
+      version='0.3.0.18',
       description='Bitcoin and Other cryptocurrency Library',
       long_description=readmetxt,
       classifiers=[
