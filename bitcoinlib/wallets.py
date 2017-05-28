11a83007acf935952c4faaa945950de8769187b8
Lennart Jongeneel <lennart@1200wd.com>
2017-05-28 23:03:07 +0200

    [FIX] query error in send method

--- bitcoinlib/wallets.py
+++ bitcoinlib/wallets.py
@@ -1273,7 +1273,8 @@ class HDWallet:
         if account_id is None:
             account_id = self.default_account_id
 
-        utxo_query = self._session.query(DbTransaction, DbKey.address).join(DbTransactionOutput.key). \
+        utxo_query = self._session.query(DbTransaction, DbKey.address).\
+            join(DbTransactionOutput).join(DbKey). \
             filter(DbKey.wallet_id == self.wallet_id,
                    DbKey.account_id == 0,
                    DbTransactionOutput.spend.op("IS")(False),
@@ -1437,7 +1438,7 @@ if __name__ == '__main__':
     #     donations_account = wallet.new_account()
     #     new_key8 = wallet.new_key(account_id=donations_account.account_id)
     #     wallet.info(detail=3)
-
+    #
     print("\n=== Create new Wallet with Testnet master key and account ID 99 ===")
     testnet_wallet = HDWallet.create(
         name='TestNetWallet',
@@ -1450,86 +1451,83 @@ if __name__ == '__main__':
     nk2 = testnet_wallet.new_key(account_id=99, name="Address #2")
     nkc = testnet_wallet.new_key_change(account_id=99, name="Change #1")
     nkc2 = testnet_wallet.new_key_change(account_id=99, name="Change #2")
-    testnet_wallet.updateutxos()
-    testnet_wallet.info(detail=3)
-
-    import sys
-    sys.exit()
-
-    # Three ways of getting the a HDWalletKey, with ID, address and name:
-    print(testnet_wallet.key(1).address)
-    print(testnet_wallet.key('n3UKaXBRDhTVpkvgRH7eARZFsYE989bHjw').address)
-    print(testnet_wallet.key('TestNetWallet').address)
-
-    print("\n=== Import Account Bitcoin Testnet key with depth 3 ===")
-    accountkey = 'tprv8h4wEmfC2aSckSCYa68t8MhL7F8p9xAy322B5d6ipzY5ZWGGwksJMoajMCqd73cP4EVRygPQubgJPu9duBzPn3QV' \
-                 '8Y7KbKUnaMzxnnnsSvh'
-    wallet_import2 = HDWallet.create(
-        databasefile=test_database,
-        name='Account Import',
-        key=accountkey,
-        network='testnet',
-        account_id=99)
-    wallet_import2.info(detail=3)
-    del wallet_import2
-
-    print("\n=== Create simple wallet and import some unrelated private keys ===")
-    simple_wallet = HDWallet.create(
-        name='Simple Wallet',
-        key='L5fbTtqEKPK6zeuCBivnQ8FALMEq6ZApD7wkHZoMUsBWcktBev73',
-        databasefile=test_database)
-    simple_wallet.import_key('KxVjTaa4fd6gaga3YDDRDG56tn1UXdMF9fAMxehUH83PTjqk4xCs')
-    simple_wallet.import_key('L3RyKcjp8kzdJ6rhGhTC5bXWEYnC2eL3b1vrZoduXMht6m9MQeHy')
-    simple_wallet.updateutxos()
-    simple_wallet.info(detail=3)
-    del simple_wallet
-
-    print("\n=== Create wallet with public key to generate addresses without private key ===")
-    pubkey = 'tpubDDkyPBhSAx8DFYxx5aLjvKH6B6Eq2eDK1YN76x1WeijE8eVUswpibGbv8zJjD6yLDHzVcqWzSp2fWVFhEW9XnBssFqM' \
-             'wt9SrsVeBeqfBbR3'
-    pubwal = HDWallet.create(
-        databasefile=test_database,
-        name='Import Public Key Wallet',
-        key=pubkey,
-        network='testnet',
-        account_id=0)
-    newkey = pubwal.new_key()
-    pubwal.info(detail=3)
-    del pubwal
-
-    print("\n=== Create Litecoin wallet ===")
-    litecoin_wallet = HDWallet.create(
-        databasefile=test_database,
-        name='Litecoin Wallet',
-        network='litecoin')
-    newkey = litecoin_wallet.new_key()
-    litecoin_wallet.info(detail=3)
-    del litecoin_wallet
-
-    print("\n=== Create Litecoin testnet Wallet from Mnemonic Passphrase ===")
-    # words = Mnemonic('english').generate()
-    words = 'blind frequent camera goddess pottery repair skull year mistake wrist lonely mix'
-    print("Generated Passphrase: %s" % words)
-    seed = Mnemonic().to_seed(words)
-    hdkey = HDKey().from_seed(seed, network='litecoin_testnet')
-    wallet = HDWallet.create(name='Mnemonic Wallet', network='litecoin_testnet',
-                             key=hdkey.wif(), databasefile=test_database)
-    wallet.new_key("Input", 0)
-    # wallet.updateutxos()  # TODO: fix for litecoin testnet
-    wallet.info(detail=3)
-
-    print("\n=== Test import Litecoin key in Bitcoin wallet (should give error) ===")
-    w = HDWallet.create(
-        name='Wallet Error',
-        databasefile=test_database)
-    try:
-        w.import_key(key='T43gB4F6k1Ly3YWbMuddq13xLb56hevUDP3RthKArr7FPHjQiXpp')
-    except KeyError as e:
-        print("Import litecoin key in bitcoin wallet gives an error: %s" % e)
-
-    print("\n=== Normalize BIP48 key path ===")
-    key_path = "m/44h/1'/0p/2000/1"
-    print("Raw: %s, Normalized: %s" % (key_path, normalize_path(key_path)))
+    # testnet_wallet.updateutxos()
+    # testnet_wallet.info(detail=3)
+    #
+    # # Three ways of getting the a HDWalletKey, with ID, address and name:
+    # print(testnet_wallet.key(1).address)
+    # print(testnet_wallet.key('n3UKaXBRDhTVpkvgRH7eARZFsYE989bHjw').address)
+    # print(testnet_wallet.key('TestNetWallet').address)
+    #
+    # print("\n=== Import Account Bitcoin Testnet key with depth 3 ===")
+    # accountkey = 'tprv8h4wEmfC2aSckSCYa68t8MhL7F8p9xAy322B5d6ipzY5ZWGGwksJMoajMCqd73cP4EVRygPQubgJPu9duBzPn3QV' \
+    #              '8Y7KbKUnaMzxnnnsSvh'
+    # wallet_import2 = HDWallet.create(
+    #     databasefile=test_database,
+    #     name='Account Import',
+    #     key=accountkey,
+    #     network='testnet',
+    #     account_id=99)
+    # wallet_import2.info(detail=3)
+    # del wallet_import2
+    #
+    # print("\n=== Create simple wallet and import some unrelated private keys ===")
+    # simple_wallet = HDWallet.create(
+    #     name='Simple Wallet',
+    #     key='L5fbTtqEKPK6zeuCBivnQ8FALMEq6ZApD7wkHZoMUsBWcktBev73',
+    #     databasefile=test_database)
+    # simple_wallet.import_key('KxVjTaa4fd6gaga3YDDRDG56tn1UXdMF9fAMxehUH83PTjqk4xCs')
+    # simple_wallet.import_key('L3RyKcjp8kzdJ6rhGhTC5bXWEYnC2eL3b1vrZoduXMht6m9MQeHy')
+    # simple_wallet.updateutxos()
+    # simple_wallet.info(detail=3)
+    # del simple_wallet
+    #
+    # print("\n=== Create wallet with public key to generate addresses without private key ===")
+    # pubkey = 'tpubDDkyPBhSAx8DFYxx5aLjvKH6B6Eq2eDK1YN76x1WeijE8eVUswpibGbv8zJjD6yLDHzVcqWzSp2fWVFhEW9XnBssFqM' \
+    #          'wt9SrsVeBeqfBbR3'
+    # pubwal = HDWallet.create(
+    #     databasefile=test_database,
+    #     name='Import Public Key Wallet',
+    #     key=pubkey,
+    #     network='testnet',
+    #     account_id=0)
+    # newkey = pubwal.new_key()
+    # pubwal.info(detail=3)
+    # del pubwal
+    #
+    # print("\n=== Create Litecoin wallet ===")
+    # litecoin_wallet = HDWallet.create(
+    #     databasefile=test_database,
+    #     name='Litecoin Wallet',
+    #     network='litecoin')
+    # newkey = litecoin_wallet.new_key()
+    # litecoin_wallet.info(detail=3)
+    # del litecoin_wallet
+    #
+    # print("\n=== Create Litecoin testnet Wallet from Mnemonic Passphrase ===")
+    # # words = Mnemonic('english').generate()
+    # words = 'blind frequent camera goddess pottery repair skull year mistake wrist lonely mix'
+    # print("Generated Passphrase: %s" % words)
+    # seed = Mnemonic().to_seed(words)
+    # hdkey = HDKey().from_seed(seed, network='litecoin_testnet')
+    # wallet = HDWallet.create(name='Mnemonic Wallet', network='litecoin_testnet',
+    #                          key=hdkey.wif(), databasefile=test_database)
+    # wallet.new_key("Input", 0)
+    # # wallet.updateutxos()  # TODO: fix for litecoin testnet
+    # wallet.info(detail=3)
+    #
+    # print("\n=== Test import Litecoin key in Bitcoin wallet (should give error) ===")
+    # w = HDWallet.create(
+    #     name='Wallet Error',
+    #     databasefile=test_database)
+    # try:
+    #     w.import_key(key='T43gB4F6k1Ly3YWbMuddq13xLb56hevUDP3RthKArr7FPHjQiXpp')
+    # except KeyError as e:
+    #     print("Import litecoin key in bitcoin wallet gives an error: %s" % e)
+    #
+    # print("\n=== Normalize BIP48 key path ===")
+    # key_path = "m/44h/1'/0p/2000/1"
+    # print("Raw: %s, Normalized: %s" % (key_path, normalize_path(key_path)))
 
     print("\n=== Send testbitcoins to an address ===")
     wallet_import = HDWallet('TestNetWallet', databasefile=test_database)
@@ -1540,7 +1538,7 @@ if __name__ == '__main__':
     for utxo in wallet_import.getutxos(99):
         print("%s %s (%d confirms)" % (
         utxo['address'], wallet_import.network.print_value(utxo['value']), utxo['confirmations']))
-    res = wallet_import.send_to('mxdLD8SAGS9fe2EeCXALDHcdTTbppMHp8N', 5000000, 99)
+    res = wallet_import.send_to('mxdLD8SAGS9fe2EeCXALDHcdTTbppMHp8N', 100000, 99)
     print("Send transaction result:")
     pprint(res)
 
