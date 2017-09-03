5fadd35137cc2c6d29700fe3060bcb36e7993146
Lennart Jongeneel <lennart@1200wd.com>
2017-09-03 20:01:37 +0200

    [REF] Recognise script type from address

--- bitcoinlib/transactions.py
+++ bitcoinlib/transactions.py
@@ -625,9 +625,13 @@ class Output:
             else:
                 _logger.warning("Script type %s not supported" % self.script_type)
 
-        # TODO: Recognise script type from address
-        if self.address and self.address[0] == '2':
-            self.script_type = 'p2sh'
+        if self.address and not self.script_type:
+            address_prefix = change_base(self.address, 58, 256)[0]
+            if address_prefix == self.network.prefix_address_p2sh:
+                self.script_type = 'p2sh'
+            elif address_prefix != self.network.prefix_address:
+                raise TransactionError("Address prefix '%s' not recognised, is network correctly defined?" %
+                                       address_prefix)
         if self.lock_script == b'':
             if self.script_type == 'p2pkh':
                 self.lock_script = b'\x76\xa9\x14' + self.public_key_hash + b'\x88\xac'
