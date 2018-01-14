7ae800af83c56bd56129e42c683e6d257c39abdd
Lennart Jongeneel <lennart@1200wd.com>
2018-01-14 08:47:34 +0700

    [WIP] Commandline wallet

--- tools/wallets-cli.py
+++ tools/wallets-cli.py
@@ -28,25 +28,44 @@ def wallet_open():
         if wn['parent_id']:
             continue
         print("[%d] %s %s %s" % (wn['id'], wn['name'], wn['network'], wn['owner']))
-    search_wlt = input("\nEnter ID of wallet to open or enter a name for a new wallet: ")
-
+    search_wlt = input("\nEnter ID of wallet to open, a name for a new wallet or ENTER to exit: ")
+    if not search_wlt:
+        return False
     if search_wlt.isdigit():
         search_wlt = int(search_wlt)
     if wallet_exists(search_wlt):
-        return HDWallet(search_wlt)
+        wlt = HDWallet(search_wlt)
+        print("Opening wallet '%s'" % wlt.name)
+        wlt.utxos_update()
+        return wlt
     else:
         print("Create wallet", search_wlt)
-        # TODO
+        # TODO: Create 
 
 
 if __name__ == "__main__":
     while True:
         wlt = wallet_open()
+        if not wlt:
+            break
         while wlt is not None:
             print("Wallet %s" % wlt.name)
-            action = input("\nWallet action? [I]nfo, [S]end, [R]eceive, [W]allet setup [C]lose: ").lower()
-            if action == 'i':
+            action = input("\nWallet action? [U[pdate, [I]nfo, [S]end, [R]eceive, [C]lose: ").lower()
+            if action == 'u':
+                print("\nUpdating UTXOs")
+                n_utxos = wlt.utxos_update()
+                print("%d new utxo's found" % n_utxos)
+            elif action == 'i':
                 print(wlt.info())
+            elif action == 's':
+                to_addr = input("\nDestination address?: ")
+                # TODO: check address
+                amount = input("Amount in %s (max: ")
+            elif action == 'r':
+                addr = wlt.get_key().address
+                print("\nReceive address is %s" % addr)
+                if QRCODES_AVAILABLE:
+                    qrcode = pyqrcode.create(addr)
+                    print(qrcode.terminal())
             elif action == 'c':
                 wlt = None
-4
