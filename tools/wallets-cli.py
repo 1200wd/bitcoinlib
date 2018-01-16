847fd2c905b4786cbaa2241a26a0d4614db5b8c4
Lennart Jongeneel <lennart@1200wd.com>
2018-01-16 16:14:11 +0700

    [ADD] Update utxo method

--- tools/wallets-cli.py
+++ tools/wallets-cli.py
@@ -10,12 +10,12 @@
 from bitcoinlib.wallets import HDWallet, wallets_list, wallet_exists
 from bitcoinlib.mnemonic import Mnemonic
 from bitcoinlib.keys import HDKey
+from bitcoinlib.services.services import Service, ServiceError
 try:
     import pyqrcode
     QRCODES_AVAILABLE = True
 except:
     QRCODES_AVAILABLE = False
-
 
 wallets_list = wallets_list()
 print("Welcome to the Bitcoinlib Command line Wallet manager")
@@ -36,11 +36,21 @@ def wallet_open():
     if wallet_exists(search_wlt):
         wlt = HDWallet(search_wlt)
         print("Opening wallet '%s'" % wlt.name)
-        wlt.utxos_update()
+        update_utxos(wlt)
         return wlt
     else:
         print("Create wallet", search_wlt)
         # TODO: Create 
+
+
+def update_utxos(wlt):
+    print("\nUpdating UTXOs")
+    try:
+        n_utxos = wlt.utxos_update()
+        print("%d new utxo's found" % n_utxos)
+    except:
+        print("Error updating UTXO's")
+        # print(e)
 
 
 if __name__ == "__main__":
@@ -49,18 +59,25 @@ if __name__ == "__main__":
         if not wlt:
             break
         while wlt is not None:
-            print("Wallet %s" % wlt.name)
-            action = input("\nWallet action? [U[pdate, [I]nfo, [S]end, [R]eceive, [C]lose: ").lower()
+            srv = Service(network=wlt.network.network_name)
+            print("\n== Wallet %s ==" % wlt.name)
+            action = input("Wallet action? [U[pdate, [I]nfo, [S]end, [R]eceive, [C]lose: ").lower()
             if action == 'u':
-                print("\nUpdating UTXOs")
-                n_utxos = wlt.utxos_update()
-                print("%d new utxo's found" % n_utxos)
+                update_utxos(wlt)
             elif action == 'i':
                 print(wlt.info())
             elif action == 's':
+                wlt_balance = wlt.balance()
+                if not wlt_balance:
+                    print("Balance is 0, no UTXO's to spent")
+                    continue
                 to_addr = input("\nDestination address?: ")
                 # TODO: check address
-                amount = input("Amount in %s (max: ")
+                update_utxos(wlt)
+                amount = input("Amount in %s (max: %d)" % (wlt.network.currency_code, wlt_balance))
+                estimated_fee = srv.estimatefee()
+                user_fee = input("Fee in smallest denominator? [%d]" % estimated_fee)
+
             elif action == 'r':
                 addr = wlt.get_key().address
                 print("\nReceive address is %s" % addr)
