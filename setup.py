2b43ed07156a432749345faf2798ce2a979dd5fa
Lennart Jongeneel <lennart@1200wd.com>
2018-02-06 23:22:00 +0100

    [FIX] Add link to CLW documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 #    BitcoinLib - Python Cryptocurrency Library
 #    PyPi Setup Tool
-#    © 2017 November - 1200 Web Development <http://1200wd.com/>
+#    © 2018 February - 1200 Web Development <http://1200wd.com/>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
