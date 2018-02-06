0131c5456f264d9455601c3aa4797220ee643b56
Lennart Jongeneel <lennart@1200wd.com>
2018-02-06 23:16:58 +0100

    [REL] New release with command line wallet

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ kwargs['install_requires'] = install_requires
 
 setup(
       name='bitcoinlib',
-      version='0.3.31a0',
+      version='0.3.32a0',
       description='Bitcoin and Other cryptocurrency Library',
       long_description=readmetxt,
       classifiers=[
