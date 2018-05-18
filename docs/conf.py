9f0de0726d765ddf2edcc85a1517dfca94e2098a
Lennart Jongeneel <lennart@1200wd.com>
2018-05-18 15:50:09 +0200

    [FIX] Exclude tools dir from sphinx to avoid hanging

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.7.0'
+# needs_sphinx = '1.7.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -171,3 +171,4 @@ def setup(app):
 
 
 autoclass_content = 'both'
+autodoc_mock_imports = ["bitcoinlib.tools"]
