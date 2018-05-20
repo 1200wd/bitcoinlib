3e8deaf06be23fe671f7f54808ec990100b03f3d
Lennart Jongeneel <lennart@1200wd.com>
2018-05-20 22:47:29 +0200

    [REF] Do not check preferred encoding on windows systems

--- bitcoinlib/main.py
+++ bitcoinlib/main.py
@@ -41,9 +41,10 @@ if not os.path.exists(DEFAULT_LOGDIR):
     os.makedirs(DEFAULT_LOGDIR)
 if not os.path.exists(DEFAULT_SETTINGSDIR):
     os.makedirs(DEFAULT_SETTINGSDIR)
-if locale.getpreferredencoding() != 'UTF-8':
+if os.name != 'nt' and locale.getpreferredencoding() != 'UTF-8':
     raise EnvironmentError("Locale is currently set to '%s'. "
-                           "This library needs the locale set to UTF-8 to function properly")
+                           "This library needs the locale set to UTF-8 to function properly" %
+                           locale.getpreferredencoding())
 
 
 # Copy data and settings to default settings directory if install.log is not found
