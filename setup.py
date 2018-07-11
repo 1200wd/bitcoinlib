60abf732cb50e304df9c3e2e3d6951faa53f5265
Lennart Jongeneel <lennart@1200wd.com>
2018-07-11 14:33:01 +0200

    [UPD] Version to 0.3.37 with full Litecoin / Dash support and some SegWit preparation

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ kwargs['install_requires'] = install_requires
 
 setup(
       name='bitcoinlib',
-      version='0.3.36',
+      version='0.3.37',
       description='Bitcoin and Other cryptocurrency Library',
       long_description=readmetxt,
       classifiers=[
