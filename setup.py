1c55df3c56fdc359f5a7073d108564e26e621ce8
Lennart Jongeneel <lennart@1200wd.com>
2018-10-24 09:35:46 +0200

    [DEL] Support for Python 3.7, wait for binaries

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Security :: Cryptography',
             'Topic :: Office/Business :: Financial :: Accounting',
