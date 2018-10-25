a8d1126391b8a4534e529b49d634fcb69f6092dc
Lennart Jongeneel <lennart@1200wd.com>
2018-10-25 17:45:02 +0200

    [FIX] Python2 issues in script_deserialize

--- bitcoinlib/transactions.py
+++ bitcoinlib/transactions.py
@@ -233,7 +233,8 @@ def script_deserialize(script, script_types=None, locking_script=None, size_byte
                     break
                 cur_char = script[cur]
                 if sys.version < '3':
-                    cur_char = ord(script[cur])
+                    if not isinstance(script, bytearray):
+                        cur_char = ord(script[cur])
                 if ch[:4] == 'hash':
                     hash_length = 0
                     if len(ch) > 5:
