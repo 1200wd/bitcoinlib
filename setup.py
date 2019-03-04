e0d03715f3c61c6c71d729debf83dbc2032bf8e5
Lennart Jongeneel <lennart@1200wd.com>
2019-03-04 20:44:57 +0100

    [UPD] Version to 0.4.5 - Rewrite main classes

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ kwargs['install_requires'] = install_requires
 
 setup(
       name='bitcoinlib',
-      version='0.4.4',
+      version='0.4.5',
       description='Bitcoin and Other cryptocurrency Library',
       long_description=readmetxt,
       classifiers=[
