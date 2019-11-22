f24f46e8f54413280b3a8c05cdfde06fb8084c50
Lennart Jongeneel <lennart@1200wd.com>
2019-11-22 20:28:31 +0100

    [REF] Try fixing readthedocs build, updating requirements

--- docs/conf.py
+++ docs/conf.py
@@ -87,8 +87,7 @@ todo_include_todos = False
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
+# further.  For a list of options available for each theme, see the# documentation.
 #
 # html_theme_options = {}
 
