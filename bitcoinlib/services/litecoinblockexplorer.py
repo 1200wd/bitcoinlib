3a3ca0449c79771170ea4e6c8348551d961931c2
Lennart <lennart@1200wd.com>
2020-05-14 10:15:47 +0200

    [REF] Remove litecoinblockexplorere isspent method

--- bitcoinlib/services/litecoinblockexplorer.py
+++ bitcoinlib/services/litecoinblockexplorer.py
@@ -181,7 +181,3 @@ class LitecoinBlockexplorerClient(BaseClient):
             'limit': limit
         }
         return block
-
-    def isspent(self, txid, output_n):
-        t = self.gettransaction(txid)
-        return t.outputs[output_n].spent
