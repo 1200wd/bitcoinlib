20bb662e6387f2ea96c48f4e15d63045fbd9df82
Lennart <lennart@1200wd.com>
2020-06-12 22:18:47 +0200

    [REF] Parse raw block and allow to limit amount of txs to parse

--- bitcoinlib/block.py
+++ bitcoinlib/block.py
@@ -25,7 +25,7 @@ from bitcoinlib.transactions import transaction_deserialize
 class Block:
 
     def __init__(self, blockhash, version, prev_block, merkle_root, timestamp, bits, nonce, transactions=None,
-                 block_height=None):
+                 block_height=None, network=DEFAULT_NETWORK):
         self.blockhash = blockhash
         self.version = version
         self.version_int = struct.unpack('>L', version)[0]
@@ -39,6 +39,7 @@ class Block:
         self.transactions = transactions
         self.txs_data = None
         self.block_height = block_height
+        self.network = network
         if not block_height and len(self.transactions):
             # first bytes of unlocking script of coinbase transaction contain block height
             self.block_height = struct.unpack('<L', self.transactions[0].inputs[0].unlocking_script[1:4] + b'\x00')[0]
@@ -47,34 +48,45 @@ class Block:
         return "<Block (%s, %d, transactions: %d" % (to_hexstring(self.blockhash), self.block_height, len(self.transactions))
 
     @classmethod
-    def from_raw(cls, rawblock, blockhash=None, parse_transactions=True, verify=True):
+    def from_raw(cls, rawblock, blockhash=None, parse_transactions=True, network=DEFAULT_NETWORK):
+        blockhash_calc = double_sha256(rawblock[:80])[::-1]
         if not blockhash:
-            blockhash = double_sha256(rawblock[:80])[::-1]
+            blockhash = blockhash_calc
+        elif blockhash != blockhash_calc:
+            raise ValueError("Provided blockhash does not correspond to calculated blockhash %s" % blockhash_calc)
+
         version = rawblock[0:4][::-1]
         prev_block = rawblock[4:36][::-1]
         merkle_root = rawblock[36:68][::-1]
         timestamp = rawblock[68:72][::-1]
         bits = rawblock[72:76][::-1]
         nonce = rawblock[76:80][::-1]
-
         tx_count, size = varbyteint_to_int(rawblock[80:89])
         txs_data = rawblock[80+size:]
-        txs = []
-        coinbase_tx = None
 
-        while (parse_transactions or not coinbase_tx) and txs_data:
-            t = transaction_deserialize(txs_data, network='bitcoin', check_size=False)
-            txs.append(t)
+        # Parse coinbase transaction so we can extract extra information
+        transactions = [transaction_deserialize(txs_data, network=network, check_size=False)]
+        txs_data = txs_data[transactions[0].size:]
+
+        while parse_transactions and txs_data:
+            t = transaction_deserialize(txs_data, network=network, check_size=False)
+            transactions.append(t)
             txs_data = txs_data[t.size:]
-            if not coinbase_tx:
-                coinbase_tx = t
             # TODO: verify transactions, need input value from previous txs
             # if verify and not t.verify():
             #     raise ValueError("Could not verify transaction %s in block %s" % (t.txid, blockhash))
 
-        block = cls(blockhash, version, prev_block, merkle_root, timestamp, bits, nonce, transactions=txs)
+        block = cls(blockhash, version, prev_block, merkle_root, timestamp, bits, nonce, transactions, network=network)
         block.txs_data = txs_data
         return block
+
+    def parse_transactions(self, limit=0):
+        n = 0
+        while self.txs_data and (limit == 0 or n < limit):
+            t = transaction_deserialize(self.txs_data, network=self.network, check_size=False)
+            self.transactions.append(t)
+            self.txs_data = self.txs_data[t.size:]
+            n += 1
 
     def as_dict(self):
         return {
