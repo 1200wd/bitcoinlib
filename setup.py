6d35899d58c9b2c2a4dfd7d2104ed264594e6335
Lennart <lennart@1200wd.com>
2020-06-23 18:48:27 +0200

    [UPD] Version to 0.4.16 with Blocks support

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ import os
 import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
-version = '0.4.15'
+version = '0.4.16'
 
 # Get the long description from the relevant file
 readmetxt = ''
