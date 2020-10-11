81bf8edf90c6f5eceae84003076f2a91baf8463b
Lennart <lennart@1200wd.com>
2020-10-11 19:32:15 +0200

    [FIX] address.address is not a method

--- bitcoinlib/wallets.py
+++ bitcoinlib/wallets.py
@@ -421,8 +421,8 @@ class HDWalletKey(object):
                 session.commit()
                 return HDWalletKey(wk.id, session, k)
 
-            nk = DbKey(name=name[:80], wallet_id=wallet_id, public=k.public_byte, private=k.private_byte, purpose=purpose,
-                       account_id=account_id, depth=k.depth, change=change, address_index=k.child_index,
+            nk = DbKey(name=name[:80], wallet_id=wallet_id, public=k.public_byte, private=k.private_byte,
+                       purpose=purpose, account_id=account_id, depth=k.depth, change=change, address_index=k.child_index,
                        wif=k.wif(witness_type=witness_type, multisig=multisig, is_private=True), address=address,
                        parent_id=parent_id, compressed=k.compressed, is_private=k.is_private, path=path,
                        key_type=key_type, network_name=network, encoding=encoding, cosigner_id=cosigner_id)
@@ -1684,7 +1684,7 @@ class HDWallet(object):
             script_type = 'p2sh_p2wsh'
         address = Address(redeemscript, encoding=self.encoding, script_type=script_type, network=network)
         already_found_key = self._session.query(DbKey).filter_by(wallet_id=self.wallet_id,
-                                                                 address=address.address()).first()
+                                                                 address=address.address).first()
         if already_found_key:
             return self.key(already_found_key.id)
         path = [pubk.path for pubk in public_keys if pubk.wallet.cosigner_id == self.cosigner_id][0]
@@ -1695,7 +1695,7 @@ class HDWallet(object):
         multisig_key = DbKey(
             name=name[:80], wallet_id=self.wallet_id, purpose=self.purpose, account_id=account_id,
             depth=depth, change=change, address_index=address_index, parent_id=0, is_private=False, path=path,
-            public=address.hash_bytes, wif='multisig-%s' % address, address=address.address(), cosigner_id=cosigner_id,
+            public=address.hash_bytes, wif='multisig-%s' % address, address=address.address, cosigner_id=cosigner_id,
             key_type='multisig', network_name=network)
         self._session.add(multisig_key)
         self._commit()
