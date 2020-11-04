f119ffa45822d53c03afbc87fedf4608f5949de9
Lennart <lennart@1200wd.com>
2020-11-04 21:47:32 +0100

    [ADD] Unittest for cache_db creation and drop_db

--- tests/test_db.py
+++ tests/test_db.py
@@ -19,13 +19,17 @@
 #
 
 import unittest
+from sqlalchemy.exc import OperationalError
 from tests.db_0_5 import Db as DbInitOld
 from bitcoinlib.db import *
+from bitcoinlib.db_cache import *
 from bitcoinlib.wallets import Wallet, WalletError
+from bitcoinlib.services.services import Service
 
 
 DATABASEFILE_UNITTESTS = os.path.join(str(BCL_DATABASE_DIR), 'bitcoinlib.unittest.sqlite')
 DATABASEFILE_TMP = os.path.join(str(BCL_DATABASE_DIR), 'bitcoinlib.tmp.sqlite')
+DATABASEFILE_CACHE_TMP = os.path.join(str(BCL_DATABASE_DIR), 'bitcoinlib_cache.tmp.sqlite')
 
 
 class TestDb(unittest.TestCase):
@@ -34,6 +38,9 @@ class TestDb(unittest.TestCase):
     def setUpClass(cls):
         if os.path.isfile(DATABASEFILE_TMP):
             os.remove(DATABASEFILE_TMP)
+        if os.path.isfile(DATABASEFILE_CACHE_TMP):
+            os.remove(DATABASEFILE_CACHE_TMP)
+
 
     def test_database_upgrade(self):
         if os.path.isfile(DATABASEFILE_UNITTESTS):
@@ -53,6 +60,19 @@ class TestDb(unittest.TestCase):
                                 Wallet.create, 'tmpwallet', db_uri=DATABASEFILE_TMP)
         dbtmp.drop_db(yes_i_am_sure=True)
         Wallet.create("tmpwallet", db_uri=DATABASEFILE_TMP)
+
+    def test_database_cache_create_drop(self):
+        dbtmp = DbCache(DATABASEFILE_CACHE_TMP)
+        self.assertEqual(dbtmp.o.path, DATABASEFILE_CACHE_TMP)
+        srv = Service(cache_uri=DATABASEFILE_CACHE_TMP, exclude_providers=['bitaps', 'bitgo'])
+        t = srv.gettransaction('68104dbd6819375e7bdf96562f89290b41598df7b002089ecdd3c8d999025b13')
+        if t:
+            self.assertGreaterEqual(srv.results_cache_n, 0)
+            srv.gettransaction('68104dbd6819375e7bdf96562f89290b41598df7b002089ecdd3c8d999025b13')
+            self.assertGreaterEqual(srv.results_cache_n, 1)
+            dbtmp.drop_db()
+            self.assertRaisesRegex(OperationalError, "", srv.gettransaction,
+                                   '68104dbd6819375e7bdf96562f89290b41598df7b002089ecdd3c8d999025b13')
 
 
 if __name__ == '__main__':
