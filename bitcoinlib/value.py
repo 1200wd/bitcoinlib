3904dd2cec47fec5d0681b185db5dbe817122e0d
Lennart <lennart@1200wd.com>
2020-11-13 13:11:33 +0100

    [REF] Import values of various denominators

--- bitcoinlib/value.py
+++ bitcoinlib/value.py
@@ -19,31 +19,52 @@
 #
 
 from bitcoinlib.networks import *
+from bitcoinlib.config.config import NETWORK_DENOMINATORS
 
 
 class Value:
 
-    def __init__(self, value, network=None):
+    def __init__(self, value, symbol=None, currency=None, denominator=1, network=DEFAULT_NETWORK):
         self.network = network
-        self.value = value
-        if isinstance(value, str):
-            value, cur = value.split()
-            self.value = float(value)
+        if not isinstance(network, Network):
+            self.network = Network(network)
+        self.symbol = symbol
+        self.currency = currency if currency else self.network.currency_name
+        self.denominator = denominator if denominator else self.network.denominator
+        if not isinstance(value, str):
+            dens = [den for den, symb in NETWORK_DENOMINATORS.items() if symb == symbol]
+            if dens:
+                self.denominator = dens[0]
+            self.value = float(value) * (self.denominator / self.network.denominator)
+        else:
+            value_items = value.split()
+            value = value_items[0]
+            cur = self.currency
+            if len(value_items) > 1:
+                cur = value_items[1]
             network_names = [n for n in NETWORK_DEFINITIONS if
                              NETWORK_DEFINITIONS[n]['currency_code'] == cur.upper()]
+            denominator = 1
             if network_names:
                 self.network = Network(network_names[0])
-
-        if not self.network:
-            self.network = DEFAULT_NETWORK
-
-        if not isinstance(self.network, Network):
-            self.network = Network(network)
+                self.currency = cur
+            else:
+                for den, symb in NETWORK_DENOMINATORS.items():
+                    if len(symb) and cur[:len(symb)] == symb:
+                        cur = self.currency[len(symb):]
+                        network_names = [n for n in NETWORK_DEFINITIONS if
+                                         NETWORK_DEFINITIONS[n]['currency_code'] == cur.upper()]
+                        if network_names:
+                            self.network = Network(network_names[0])
+                            self.currency = cur
+                        denominator = den
+                        break
+            self.value = float(value) * (denominator / self.network.denominator)
 
     def __str__(self):
         return self.str()
 
-    def str(self):
+    def str(self, symbol=None):
         symb = self.network.currency_code
         denominator = self.network.denominator
         denominator_size = -int(math.log10(denominator))
