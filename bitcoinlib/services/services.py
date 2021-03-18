560d25b683ec33b4635cf9dcc74eb0dd9e5a97b1
Lennart <lennart@1200wd.com>
2021-03-18 21:19:05 +0100

    [UPD] Add travis test for python 3.9 and ubuntu focal

--- bitcoinlib/services/services.py
+++ bitcoinlib/services/services.py
@@ -2,7 +2,7 @@
 #
 #    BitcoinLib - Python Cryptocurrency Library
 #    SERVICES - Main Service connector
-#    © 2017 - 2019 August - 1200 Web Development <http://1200wd.com/>
+#    © 2017 - 2021 March - 1200 Web Development <http://1200wd.com/>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
--- bitcoinlib/transactions.py
+++ bitcoinlib/transactions.py
@@ -2,7 +2,7 @@
 #
 #    BitcoinLib - Python Cryptocurrency Library
 #    TRANSACTION class to create, verify and sign Transactions
-#    © 2017 - 2020 October - 1200 Web Development <http://1200wd.com/>
+#    © 2017 - 2021 March - 1200 Web Development <http://1200wd.com/>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
