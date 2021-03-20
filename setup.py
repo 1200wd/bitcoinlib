bfc1e5e98dda2676840d078b38731716f8a6de00
Lennart <lennart@1200wd.com>
2021-03-20 09:58:19 +0100

    [UPD] User older numpy version which works for python 3.6

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ install_requires = [
       'pyaes==1.6.1',
       'scrypt==0.8.17',
       'SQLAlchemy==1.3.22',
-      'numpy==1.20.1',
+      'numpy==0.19,5',
 ]
 
 kwargs['install_requires'] = install_requires
