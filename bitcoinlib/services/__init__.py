cad3085cea1c68624ebe6c1ead21eaf9dad7e418
Lennart <lennart@1200wd.com>
2021-06-15 00:29:04 +0200

    [REF] Temp. disable chainso provider

--- bitcoinlib/services/__init__.py
+++ bitcoinlib/services/__init__.py
@@ -38,4 +38,4 @@ import bitcoinlib.services.insightdash
 import bitcoinlib.services.smartbit
 import bitcoinlib.services.blockstream
 import bitcoinlib.services.blocksmurfer
-import bitcoinlib.services.chainso
+# import bitcoinlib.services.chainso
