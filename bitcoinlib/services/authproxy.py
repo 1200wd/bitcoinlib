74291790b64445ec7e1a44e2431a784b4495d326
merlin mary john <merlinmary3.14@gmail.com>
2022-09-19 19:36:46 +0530

    removing port fix

--- bitcoinlib/services/authproxy.py
+++ bitcoinlib/services/authproxy.py
@@ -149,9 +149,7 @@ class AuthServiceProxy(object):
                 'Authorization': self.__auth_header,
                 'Content-type': 'application/json'
             }
-
-        url_path = self.__url.path.split(':')[0]
-        self.__conn.request('POST', url_path, postdata, headers)
+        self.__conn.request('POST', self.__url.path, postdata, headers)
         self.__conn.sock.settimeout(self.__timeout)
 
         response = self._get_response()
