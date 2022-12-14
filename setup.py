75fe7d5a1d2b3b86a88ead30f72adadb542d2664
Lennart <lennart@1200wd.com>
2022-12-14 10:50:51 +0100

    [REL] RELEASE 0.6.7 - Fix installation, dependencies and provider issues

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ import os
 import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
-version = '0.6.6'
+version = '0.6.7'
 
 # Get the long description from the relevant file
 readmetxt = ''
