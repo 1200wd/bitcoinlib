bd2cb442d2c11828e44226151d640ffedca4a461
Lennart <lennart@1200wd.com>
2023-07-05 20:26:18 +0200

    [DEL] Remove configuration specific checks from bitcoind and litecoind

--- bitcoinlib/services/bitcoind.py
+++ bitcoinlib/services/bitcoind.py
@@ -132,13 +132,6 @@ class BitcoindClient(BaseClient):
             bdc = self.from_config('', network)
             base_url = bdc.base_url
             network = bdc.network
-        if len(base_url.split(':')) != 4:
-            raise ConfigError("Bitcoind connection URL must be of format 'http(s)://user:password@host:port,"
-                              "current format is %s. Please set url in providers.json file or check bitcoin config "
-                              "file" % base_url)
-        if 'password' in base_url:
-            raise ConfigError("Invalid password in bitcoind provider settings. "
-                              "Please replace default password and set url in providers.json or bitcoin.conf file")
         _logger.info("Connect to bitcoind")
         self.proxy = AuthServiceProxy(base_url)
         super(self.__class__, self).__init__(network, PROVIDERNAME, base_url, denominator, *args)
