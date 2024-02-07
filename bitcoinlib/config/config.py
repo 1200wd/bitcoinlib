4546bc1519cb93852f0c30e63e3385444aa887d1
Cryp Toon <info@1200wd.com>
2024-02-07 18:29:49 +0100

    Fix link to sqlite database for testing

--- bitcoinlib/config/config.py
+++ bitcoinlib/config/config.py
@@ -255,7 +255,8 @@ def read_config():
     BCL_DATABASE_DIR.mkdir(parents=True, exist_ok=True)
     default_databasefile = DEFAULT_DATABASE = \
         config_get('locations', 'default_databasefile', fallback='bitcoinlib.sqlite')
-    if not default_databasefile.startswith('postgresql') or default_databasefile.startswith('mysql'):
+    if not (default_databasefile.startswith('postgresql') or default_databasefile.startswith('mysql') or
+            default_databasefile.startswith('mariadb')):
         DEFAULT_DATABASE = str(Path(BCL_DATABASE_DIR, default_databasefile))
     default_databasefile_cache = DEFAULT_DATABASE_CACHE = \
         config_get('locations', 'default_databasefile_cache', fallback='bitcoinlib_cache.sqlite')
