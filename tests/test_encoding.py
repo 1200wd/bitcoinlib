0e70f48ec8eb85145d37c872d405a01bce895f00
Lennart Jongeneel <lennart@1200wd.com>
2024-02-15 21:51:41 +0100

    Add bech32m unittest for valid addresses

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -266,14 +266,18 @@ VALID_ADDRESS = [
     ["BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4", "0014751e76e8199196d454941c45d1b3a323f1433bd6"],
     ["tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
      "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262"],
-    # Invalid checksum according to new bech32m definition
-    # ["bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx",
-    #  "5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6"],
-    # ["BC1SW50QA3JX3S", "6002751e"],
-    # ["bc1zw508d6qejxtdg4y5r3zarvaryvg6kdaj", "5210751e76e8199196d454941c45d1b3a323"],
-    ["tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
-     "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433"],
+    ['bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y',
+     '5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6'],
+    ['BC1SW50QGDZ25J', '6002751e'],
+    ['bc1zw508d6qejxtdg4y5r3zarvaryvaxxpcs', '5210751e76e8199196d454941c45d1b3a323'],
+    ['tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy',
+     '0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433'],
+    ['tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c',
+     '5120000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433'],
+    ['bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0',
+     '512079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798']
 ]
+
 
 INVALID_ADDRESS = [
     "tc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty",
@@ -374,10 +378,7 @@ class TestEncodingBech32SegwitAddresses(unittest.TestCase):
     def test_valid_address(self):
         """Test whether valid addresses decode to the correct output."""
         for (address, hexscript) in VALID_ADDRESS:
-            try:
-                scriptpubkey = addr_bech32_to_pubkeyhash(address, include_witver=True)
-            except EncodingError:
-                scriptpubkey = addr_bech32_to_pubkeyhash(address, prefix='tb', include_witver=True)
+            scriptpubkey = addr_bech32_to_pubkeyhash(address, include_witver=True)
             self.assertEqual(scriptpubkey, bytes.fromhex(hexscript))
             addr = pubkeyhash_to_addr_bech32(scriptpubkey, address[:2].lower())
             self.assertEqual(address.lower(), addr)
