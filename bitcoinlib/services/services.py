977f3008fb247f7e94d943f569835f78ba7a9aeb
Cryp Toon <info@1200wd.com>
2026-06-03 12:36:13 +0200

    Always get new transactions, even if blockheight is unchanged

--- bitcoinlib/services/services.py
+++ bitcoinlib/services/services.py
@@ -383,14 +383,12 @@ class Service(object):
                 qry_after_txid = bytes.fromhex(txs_cache[-1:][0].txid)
 
         # Get (extra) transactions from service providers
-        txs = []
-        if not (db_addr and db_addr.last_block and db_addr.last_block >= self.blockcount()) or not caching_enabled:
-            txs = self._provider_execute('gettransactions', address, qry_after_txid.hex(),  limit)
-            if txs is False:
-                raise ServiceError("Error when retrieving transactions from service provider")
-            for tx in txs:
-                if tx.date and not tx.date.tzinfo:
-                    tx.date = tx.date.replace(tzinfo=timezone.utc)
+        txs = self._provider_execute('gettransactions', address, qry_after_txid.hex(),  limit)
+        if txs is False:
+            raise ServiceError("Error when retrieving transactions from service provider")
+        for tx in txs:
+            if tx.date and not tx.date.tzinfo:
+                tx.date = tx.date.replace(tzinfo=timezone.utc)
 
         # Store transactions and address in cache
         # - disable cache if comparing providers or if after_txid is used and no cache is available
